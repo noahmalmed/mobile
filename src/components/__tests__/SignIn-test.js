9fb490f0682760babc02973a49bd254687a4f721
Noah Malmed <nmalmed@adlerplanetarium.org>
2018-11-19 09:29:58 -0600

    Update Tests
    Most of these test updates are for Font updates

--- src/components/__tests__/SignIn-test.js
+++ src/components/__tests__/SignIn-test.js
@@ -3,16 +3,20 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import { SignIn } from '../SignIn'
 
+const navBarActions = {
+  setNavbarBackButton: () => {}
+}
+
 it('renders correctly', () => {
   const tree = renderer.create(
-    <SignIn isConnected={true} />
+    <SignIn isConnected={true} navBarActions={navBarActions} />
   ).toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 it('renders error message', () => {
   const tree = renderer.create(
-    <SignIn isConnected={true} errorMessage={'does not compute'} />
+    <SignIn navBarActions={navBarActions} isConnected={true} errorMessage={'does not compute'} />
   ).toJSON()
   expect(tree).toMatchSnapshot()
 })
